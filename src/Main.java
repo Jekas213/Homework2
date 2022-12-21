aadecdbbf46d539a1cfef393317877935448a0b1
Jekas <jekas213@gmail.com>
2022-12-21 20:44:11 +0300

    задачи 4-5

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
     }
 
     public static void task1 () {
@@ -49,5 +56,77 @@ public class Main {
         paper = paper - 7639;
         System.out.println(paper);
         // Пишем код для задачи 3
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+
+
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+
+
+
+
+    }
+
+    public static void task6 () {
+        System.out.println("Задача 6");
+        var dog = 8;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        System.out.println(cat);
+
+        var paper = 763789;
+        System.out.println(paper);
+
+
+    }
+
+    public static void task7 () {
+        System.out.println("Задача 7");
+        var dog = 8;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        System.out.println(cat);
+
+        var paper = 763789;
+        System.out.println(paper);
+
+
+    }
+
+    public static void task8 () {
+        System.out.println("Задача 8");
+        var dog = 8;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        System.out.println(cat);
+
+        var paper = 763789;
+        System.out.println(paper);
+
+
     }
 }
