b1e927176230ce1e8cb2970bada57a9a88be57a8
Jekas <jekas213@gmail.com>
2022-12-21 20:31:42 +0300

    задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,52 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1 () {
-        System.out.println("Задача 1");
-         // Пишем код для задачи 1
+        var dog = 8;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        System.out.println(cat);
+
+        var paper = 763789;
+        System.out.println(paper);
+
+
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
+        var dog = 8;
+        dog = dog + 4;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        cat = cat + 4;
+        System.out.println(cat);
+
+        var paper = 763789;
+        paper = paper + 4;
+        System.out.println(paper);
         // Пишем код для задачи 2
+    }
+
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+        double dog = 8;
+        dog = dog - 3.5;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        cat = cat - 1.6;
+        System.out.println(cat);
+
+        var paper = 763789;
+        paper = paper - 7639;
+        System.out.println(paper);
+        // Пишем код для задачи 3
     }
 }
